39af6443870641138af953cf0765df281bd2977d
BeFreel <nepogodin_p@mail.ru>
2020-03-19 15:57:38 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ function eval() {
     return;
 }
 
+function check(str, bracketsConfig) {
+    const brackets = bracketsConfig.map((el) => el.join(""));
+    for(let i = 0; i < brackets.length; i++) {
+      if(str.includes(brackets[i])) {
+        str = str.replace(brackets[i], "");
+        i = -1;
+      }
+    }
+    return (str.length)? false : true;
+}
+
 function expressionCalculator(expr) {
-    // write your solution here
+    if(!check(expr.replace(/[^()]/g, ""), [["(", ")"]]))
+        throw new TypeError('ExpressionError: Brackets must be paired');
+    return (new Function(`
+        let res = ${expr};
+        if (res === Infinity)
+        throw new TypeError('TypeError: Division by zero.');
+        else
+          return res;
+    `))();
 }
 
 module.exports = {
